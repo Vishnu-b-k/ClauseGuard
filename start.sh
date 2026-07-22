1a568ed8b61e25248aa4be5018bf02984b6aeaaf
Vishnu-b-k <151603829+Vishnu-b-k@users.noreply.github.com>
2026-07-22 23:21:58 +0530

    feat: add start.sh to run celery and uvicorn in same container

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Start the Celery worker in the background
+echo "Starting Celery Background Worker..."
+celery -A src.worker.celery_app worker --loglevel=info &
+
+# Start the FastAPI Web Service in the foreground
+echo "Starting FastAPI Web Service..."
+uvicorn src.api.app:app --host 0.0.0.0 --port $PORT
